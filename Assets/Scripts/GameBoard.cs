0a53dac8f382772200a7d10fa4b79983bb2899bd
PalomaFerrara <palomamorenaferrara@gmail.com>
2026-09-11 15:21:44 -0300

    script

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class Casilleros : MonoBehaviour
+{
+    public Vector3[] posiciones =
+    {
+        // Casillero 1
+        new Vector3(4.37099981f, 0.328000009f, 4.375f),
+
+        // Casillero 2
+        new Vector3(4.37099981f, 0.328000009f, 3.05299997f),
+
+        // Casillero 3
+        new Vector3(4.37099981f, 0.328000009f, 1.84800005f),
+
+        // Casillero 4
+        new Vector3(4.37099981f, 0.328000009f, 0.582000017f),
+
+        // Casillero 5
+        new Vector3(4.37099981f, 0.328000009f, -0.620000005f),
+
+        // Casillero 6
+        new Vector3(4.37099981f, 0.328000009f, -1.91999996f),
+
+        // Casillero 7
+        new Vector3(4.37099981f, 0.328000009f, -3.18000007f),
+
+        // Casillero 8
+        new Vector3(4.37099981f, 0.328000009f, -4.44000006f),
+
+        // Casillero 9
+        new Vector3(3.11999989f, 0.328000009f, -4.44000006f),
+
+        // Casillero 10
+        new Vector3(1.82000005f, 0.328000009f, -4.44000006f),
+
+        // Casillero 11
+        new Vector3(0.620000005f, 0.328000009f, -4.44000006f),
+
+        // Casillero 12
+        new Vector3(-0.689999998f, 0.328000009f, -4.44000006f),
+
+        // Casillero 13
+        new Vector3(-1.91999996f, 0.328000009f, -4.44000006f),
+
+        // Casillero 14
+        new Vector3(-3.18000007f, 0.328000009f, -4.44000006f),
+
+        // Casillero 15
+        new Vector3(-4.36999989f, 0.328000009f, -4.44000006f),
+
+        // Casillero 16
+        new Vector3(-4.36999989f, 0.328000009f, -3.1400001f),
+
+        // Casillero 17
+        new Vector3(-4.36999989f, 0.328000009f, -3.1400001f),
+
+        // Casillero 18
+        new Vector3(-4.36999989f, 0.328000009f, -0.660000026f),
+
+        // Casillero 19
+        new Vector3(-4.36999989f, 0.328000009f, 0.610000014f),
+
+        // Casillero 20
+        new Vector3(-4.36999989f, 0.328000009f, 1.83000004f),
+
+        // Casillero 21
+        new Vector3(-4.36999989f, 0.328000009f, 3.06999993f),
+
+        // Casillero 22
+        new Vector3(-4.36999989f, 0.328000009f, 4.26999998f),
+
+        // Casillero 23
+        new Vector3(-3.13000011f, 0.328000009f, 4.26999998f),
+
+        // Casillero 24
+        new Vector3(-1.92999995f, 0.328000009f, 4.26999998f),
+
+        // Casillero 25
+        new Vector3(-0.649999976f, 0.328000009f, 4.26999998f),
+
+        // Casillero 26
+        new Vector3(0.589999974f, 0.328000009f, 4.26999998f),
+
+        // Casillero 27
+        new Vector3(1.85000002f, 0.328000009f, 4.26999998f),
+
+        // Casillero 28
+        new Vector3(3.07999992f, 0.328000009f, 4.26999998f)
+    };
+}
